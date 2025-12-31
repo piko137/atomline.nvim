9301a82eec4fa3cf46f9f6e9bbd7ab715de3a944
Kerwin <lua2d0@gmail.com>
2025-12-31 15:29:00 +0800

    Update init.lua

--- lua/atomline/init.lua
+++ lua/atomline/init.lua
@@ -1,43 +1,93 @@
 local M = {}
 
--- 1. 總入口：把所有功能串起來
+-- 總入口：由 ftplugin 或 config 呼叫
 function M.setup()
   M.apply_syntax()
   
   local set = vim.opt_local
   set.expandtab = true
   set.shiftwidth = 2
+  set.softtabstop = 2
   set.commentstring = "# %s"
   set.foldmethod = "expr"
   set.foldexpr = "v:lua.require'atomline'.fold_expr(v:lnum)"
   set.foldlevel = 99
 
   local opts = { buffer = true, silent = true }
-  vim.keymap.set('n', '<leader>x', M.toggle_status, { buffer = true, desc = "Toggle Status" })
-  vim.keymap.set('n', '<leader>ts', M.insert_timestamp, { buffer = true, desc = "Timestamp" })
+  -- 快捷鍵設定
+  vim.keymap.set('n', '<leader>x', M.toggle_status, { buffer = true, desc = "Toggle AtomLine Status" })
+  vim.keymap.set('n', '<leader>ts', M.insert_timestamp, { buffer = true, desc = "Insert Timestamp" })
   vim.keymap.set('i', '<CR>', M.smart_newline, { buffer = true, expr = true })
   vim.keymap.set('n', 'za', 'za', opts)
 end
 
--- 2. 視覺邏輯
+-- 語法高亮與顏色
 function M.apply_syntax()
   vim.cmd([[syntax on]])
   local hl = vim.api.nvim_set_hl
-  hl(0, "AtomLineTodo", { fg = "#FF5555", bold = true })
-  hl(0, "AtomLineDoing", { fg = "#F1FA8C", bold = true })
-  hl(0, "AtomLineDone", { fg = "#50FA7B" })
+  
+  hl(0, "AtomLineTodo",         { fg = "#FF5555", bold = true })
+  hl(0, "AtomLineDoing",        { fg = "#F1FA8C", bold = true })
+  hl(0, "AtomLineDone",         { fg = "#50FA7B" })
+  hl(0, "AtomLineMigrate",      { fg = "#BD93F9" })
   hl(0, "AtomLineContinuation", { fg = "#6272a4" })
-  -- ... (其餘 hl 定義請維持原樣) ...
+  hl(0, "AtomLineComment",      { fg = "#44475a", italic = true })
+  hl(0, "AtomLineTag",          { fg = "#FF79C6" })
+  hl(0, "AtomLinePerson",       { fg = "#8BE9FD" })
+  hl(0, "AtomLinePlace",        { fg = "#FFB86C" })
+  hl(0, "AtomLineDeadline",     { fg = "#FF5555", underline = true })
+  hl(0, "AtomLineSeparator",    { fg = "#6272a4", italic = true })
 
   vim.cmd([[
     syntax match AtomLineTodo "\[\.\]"
     syntax match AtomLineDoing "\[/\]"
     syntax match AtomLineDone "\[x\]"
+    syntax match AtomLineMigrate "\[>\]"
     syntax match AtomLineContinuation "^\.\..*$"
-    -- ... (其餘 syntax match 請維持原樣) ...
+    syntax match AtomLineComment "^#.*$"
+    syntax match AtomLineTag ":[^:]\+:"
+    syntax match AtomLinePerson "\~[^ ]\+"
+    syntax match AtomLinePlace "@[^ ]\+"
+    syntax match AtomLineDeadline "![0-9-]\+"
+    syntax match AtomLineSeparator "||.*$"
   ]])
 end
 
--- ... (toggle_status, fold_expr, smart_newline 等其餘函數維持原樣) ...
+-- 狀態循環切換
+function M.toggle_status()
+  local line = vim.api.nvim_get_current_line()
+  local states = { "%[%.%]", "%[/%]", "%[x%]" }
+  local next_states = { "[.]", "[/]", "[x]" }
+  for i, s in ipairs(states) do
+    if line:find(s) then
+      local next_idx = (i % #next_states) + 1
+      vim.api.nvim_set_current_line(line:gsub(s, next_states[next_idx], 1))
+      return
+    end
+  end
+end
+
+-- 摺疊邏輯
+function M.fold_expr(lnum)
+  local line = vim.fn.getline(lnum)
+  if line:match("^%.%.") or line:match("^#") then return "1" end
+  return "0"
+end
+
+-- 智慧續行 (Insert Mode)
+function M.smart_newline()
+  local line = vim.api.nvim_get_current_line()
+  if line:find("^%[") or line:find("^%.%.") then
+    return vim.api.nvim_replace_termcodes("<CR>.. ", true, false, true)
+  else
+    return vim.api.nvim_replace_termcodes("<CR>", true, false, true)
+  end
+end
+
+-- 插入時間戳記
+function M.insert_timestamp()
+  local timestamp = os.date("%Y-%m-%d %H:%M %a | ")
+  vim.api.nvim_put({timestamp}, "c", true, true)
+end
 
 return M
