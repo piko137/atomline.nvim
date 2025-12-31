2cbdc08a44cbb061a184194767e91263111f1281
Kerwin <lua2d0@gmail.com>
2025-12-31 15:38:43 +0800

    Remove setup function and key mappings
    Removed the setup function and its associated key mappings, while retaining the syntax highlighting and other functionalities.

--- lua/atomline/init.lua
+++ lua/atomline/init.lua
@@ -1,31 +1,11 @@
 local M = {}
 
--- 總入口：由 ftplugin 或 config 呼叫
-function M.setup()
-  M.apply_syntax()
-  
-  local set = vim.opt_local
-  set.expandtab = true
-  set.shiftwidth = 2
-  set.softtabstop = 2
-  set.commentstring = "# %s"
-  set.foldmethod = "expr"
-  set.foldexpr = "v:lua.require'atomline'.fold_expr(v:lnum)"
-  set.foldlevel = 99
-
-  local opts = { buffer = true, silent = true }
-  -- 快捷鍵設定
-  vim.keymap.set('n', '<leader>x', M.toggle_status, { buffer = true, desc = "Toggle AtomLine Status" })
-  vim.keymap.set('n', '<leader>ts', M.insert_timestamp, { buffer = true, desc = "Insert Timestamp" })
-  vim.keymap.set('i', '<CR>', M.smart_newline, { buffer = true, expr = true })
-  vim.keymap.set('n', 'za', 'za', opts)
-end
-
--- 語法高亮與顏色
+-- A. 高亮邏輯
 function M.apply_syntax()
   vim.cmd([[syntax on]])
   local hl = vim.api.nvim_set_hl
   
+  -- 定義顏色 (可根據你的主題微調)
   hl(0, "AtomLineTodo",         { fg = "#FF5555", bold = true })
   hl(0, "AtomLineDoing",        { fg = "#F1FA8C", bold = true })
   hl(0, "AtomLineDone",         { fg = "#50FA7B" })
@@ -53,7 +33,7 @@ function M.apply_syntax()
   ]])
 end
 
--- 狀態循環切換
+-- B. 狀態切換邏輯
 function M.toggle_status()
   local line = vim.api.nvim_get_current_line()
   local states = { "%[%.%]", "%[/%]", "%[x%]" }
@@ -67,16 +47,17 @@ function M.toggle_status()
   end
 end
 
--- 摺疊邏輯
+-- C. 摺疊邏輯
 function M.fold_expr(lnum)
   local line = vim.fn.getline(lnum)
   if line:match("^%.%.") or line:match("^#") then return "1" end
   return "0"
 end
 
--- 智慧續行 (Insert Mode)
+-- D. 智慧續行 (Insert Mode)
 function M.smart_newline()
   local line = vim.api.nvim_get_current_line()
+  -- 若該行是任務開頭或續行開頭，按 Enter 自動補上 ".."
   if line:find("^%[") or line:find("^%.%.") then
     return vim.api.nvim_replace_termcodes("<CR>.. ", true, false, true)
   else
@@ -84,7 +65,7 @@ function M.smart_newline()
   end
 end
 
--- 插入時間戳記
+-- E. 時間戳記
 function M.insert_timestamp()
   local timestamp = os.date("%Y-%m-%d %H:%M %a | ")
   vim.api.nvim_put({timestamp}, "c", true, true)
